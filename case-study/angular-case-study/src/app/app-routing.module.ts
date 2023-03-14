3bdb5f4b81aebb1bd6bce48c8dafd24a3b25e96a
LeQuocNhatNam <lequocnhatnam07041994@gmail.com>
2023-03-14 23:25:01 +0700

    submit

--- case-study/angular-case-study/src/app/app-routing.module.ts
+++ case-study/angular-case-study/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import {Routes, RouterModule} from '@angular/router';
 import {CustomerCreateComponent} from "./component/customer/customer-create/customer-create.component";
 import {CustomerListComponent} from "./component/customer/customer-list/customer-list.component";
 import {CustomerEditComponent} from "./component/customer/customer-edit/customer-edit.component";
+import {ContractListComponent} from "./component/contract/contract-list/contract-list.component";
+import {FacilityListComponent} from "./component/facility/facility-list/facility-list.component";
 
 
 const routes: Routes = [{
@@ -16,6 +18,14 @@ const routes: Routes = [{
   {
     path: 'customer/edit',
     component: CustomerEditComponent
+  },
+  {
+    path: 'contract',
+    component: ContractListComponent
+  },
+  {
+    path: 'facility',
+    component: FacilityListComponent
   }
 ];
 
--- case-study/angular-case-study/src/app/component/contract/contract-list/contract-list.component.ts
+++ case-study/angular-case-study/src/app/component/contract/contract-list/contract-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Contract} from "../../../model/contract/contract";
 
 @Component({
   selector: 'app-contract-list',
@@ -6,8 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contract-list.component.css']
 })
 export class ContractListComponent implements OnInit {
+  contracts: Contract[] = [{
+    id: 1,
+    deposit: 0,
+    startDate: '2020-12-18',
+    endDate: '2020-12-19',
+    customer: {id: 1, name: 'Nguyễn Văn A'},
+    facility: {id: 3, name: 'House'}
+  },{
+    id: 2,
+    deposit: 0,
+    startDate: '2020-12-18',
+    endDate: '2020-12-19',
+    customer: {id: 1, name: 'Nguyễn Văn A'},
+    facility: {id: 3, name: 'House'}
+  },{
+    id: 3,
+    deposit: 0,
+    startDate: '2020-12-18',
+    endDate: '2020-12-19',
+    customer: {id: 1, name: 'Nguyễn Văn A'},
+    facility: {id: 3, name: 'House'}
+  },{
+    id: 4,
+    deposit: 200,
+    startDate: '2020-12-18',
+    endDate: '2020-12-19',
+    customer: {id: 1, name: 'Nguyễn Văn A'},
+    facility: {id: 3, name: 'House'}
+  },];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
